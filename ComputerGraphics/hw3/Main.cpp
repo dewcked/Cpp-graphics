c1191b77ba577d7b7fad241b97d480283193a313
dewcked <rkdcksdn1234@gmail.com>
2019-12-05 09:57:24 +0900

    complete textured cube

--- ComputerGraphics/hw3/Main.cpp
+++ ComputerGraphics/hw3/Main.cpp
@@ -1,115 +1,264 @@
+/****** Initilization Header - Setup & Prepares Key Game Items ******/
+
 #pragma comment (lib,"SDL2.lib")
 #pragma comment (lib,"SDL2main")
 #pragma comment (lib,"SDL2_image")
 #pragma comment (lib,"opengl32")
 
-#include <cstdio>
 #include "include/SDL.h"
 #include "include/SDL_opengl.h"
 #include "include/SDL_image.h"
-#include <fstream>
+#include <GL/glut.h>
 #include <iostream>
+#include <list>
+#include <vector>
 
-using namespace std;
 
+const int window_width = 1024;
+const int window_height = 1024;
+const int SCREEN_BPP = 32;
 
-SDL_Renderer* renderer;
+SDL_Event event;
+SDL_Window* win = NULL;
+SDL_Renderer* renderer = NULL;
+GLuint texId[6];
+SDL_GLContext context;
+/****** Name Space for Game Variables ******/
+namespace var
+{
+	int frame_start_time = 0;
+	int frame_current_time = 0;
+	int frame_count = 0;
+}
 
-void printSize(const std::string& address) {
-	std::fstream motd(address.c_str(), std::ios::binary | std::ios::in | std::ios::ate);
-	if (motd) {
-		std::fstream::pos_type size = motd.tellg();
-		std::cout << address << " " << size << "\n";
+/****** Initializes Core OpenGL Features ******/
+bool opengl_init()
+{
+	glEnable(GL_DEPTH_TEST);
+	//glDepthFunc(GL_LEQUAL);
+	
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//glEnable(GL_BLEND);
+	//glEnable(GL_ARB_texture_non_power_of_two);
+	glEnable(GL_TEXTURE_2D);
+	//glEnable(GL_TEXTURE_3D);
+	std::cout << "Allowed extensions: " << glGetString(GL_EXTENSIONS) << std::endl;
+	glClearColor(0, 0, 0, 0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (640.0 / 480.0), 0.1, 100.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	//std::cout << "Error Code: " << glGetError() << std::endl;
+	if (glGetError() != GL_NO_ERROR)
+	{
+		return false;
 	}
-	else {
-		perror(address.c_str());
+
+	return true;
+}
+
+
+/****** Initializes SDL, OpenGL and Video and Window ******/
+bool init()
+{
+
+	//if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		return false;
+	}
+
+	win = SDL_CreateWindow("20151179\xEA\xB0\x95\xEC\xB0\xAC\xEC\x9A\xB0", 100, 100, window_width, window_height, SDL_WINDOW_OPENGL);
+	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+	context = SDL_GL_CreateContext(win);
+
+	if (opengl_init() == false)
+	{
+		return false;
 	}
 }
 
-int main(int argc, char** argv) {
-	int winWidth = 800;
-	int winHeight = 600;
+/* Removes objects before game closes */
+void clean_up()
+{
+	SDL_Quit();
+}
 
-	SDL_Window* win = NULL;
-	SDL_Renderer* renderer = NULL;
-	SDL_Surface* image;
-	SDL_RWops* rwop;
-	rwop = SDL_RWFromFile("res/KGU-logo.jpg", "rb");
-	image = IMG_LoadJPG_RW(rwop);
-	GLubyte* map = (GLubyte*)image->pixels;
-
-	int w, h;
-	w = image->w;
-	h = image->h;
-
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-		return 1;
-
-	win = SDL_CreateWindow("gl", 100, 100, winWidth, winHeight, SDL_WINDOW_OPENGL);
-	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
-
-	SDL_GLContext context;
-	context = SDL_GL_CreateContext(win);
-
-
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-	glEnable(GL_TEXTURE_2D);
-
-	GLuint texId;
-	glGenTextures(1, &texId);
-	glBindTexture(GL_TEXTURE_2D, texId);
+void errorcheck() {
+	int errormsg;
+	if ((errormsg = glGetError()) != GL_NO_ERROR)
+	{
+		std::cout << "glerror: " << errormsg << std::endl;
+		exit(0);
+	}
+}
+void RenewTexture(SDL_Surface* tmpimg, int index) {
+	GLubyte* map = (GLubyte*)tmpimg->pixels;
+	glBindTexture(GL_TEXTURE_2D, texId[index]); errorcheck();
+	GLsizei w, h;
+	w = tmpimg->w;
+	h = tmpimg->h;
+	//glPixelStorei(GL_UNPACK_ALIGNMENT, texId[index]); errorcheck();
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); errorcheck();
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 6);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);//GL_CLAMP_TO_EDGE, GL_CLAMP
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	errorcheck();
+	/*gluBuild2DMipmaps(
+		GL_TEXTURE_2D, GL_RGBA8,
+		w, h,
+		GL_RGBA8, GL_UNSIGNED_BYTE,
+		map
+	);*/
 	glTexImage2D(
 		GL_TEXTURE_2D, 0, GL_RGB,
 		w, h, 0,
 		GL_RGB, GL_UNSIGNED_BYTE,
-		map
+		(GLvoid *)map
 	);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	errorcheck();
+	//glBindTexture(GL_TEXTURE_2D, texId[index]);
+}
 
+int main(int argc, char* args[])
+{
+	std::vector<SDL_Surface*> image;
+	GLfloat rotation = 0.0f;
 
-	while (1) {
-		SDL_Event e;
-		if (SDL_PollEvent(&e)) {
-			if (e.type == SDL_QUIT)
-				break;
-			else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
-				break;
-		}
+	bool quit = false;
 
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glOrtho(0, winWidth, winHeight, 0, -1, 1);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-
-		glBindTexture(GL_TEXTURE_2D, texId);
-
-		glBegin(GL_QUADS);
-		glTexCoord2f(0.0, 0.0); glVertex2f(0, 0);
-		glTexCoord2f(1.0, 0.0); glVertex2f(w, 0);
-		glTexCoord2f(1.0, 1.0); glVertex2f(w, h);
-		glTexCoord2f(0.0, 1.0); glVertex2f(0, h);
-		glEnd();
-
-		glBegin(GL_QUADS);
-		glTexCoord2f(0.0, 0.0); glVertex2f(0 + 30, 0 + 30);
-		glTexCoord2f(1.0, 0.0); glVertex2f(w + 30, 0 + 30);
-		glTexCoord2f(1.0, 1.0); glVertex2f(w + 30, h + 30);
-		glTexCoord2f(0.0, 1.0); glVertex2f(0 + 30, h + 30);
-		glEnd();
-
-
-		SDL_GL_SwapWindow(win);
-		SDL_Delay(21);
-
+	if (init() == false)
+	{
+		return 1;
 	}
 
-	SDL_GL_DeleteContext(context);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(win);
+	const char* ImageNames[6] = { "res/KGU-logo.jpg", "res/Me.jpeg", "res/beach-1838501.jpg", "res/bench-560435.jpg", "res/wolf-1341881_1920.jpg", "res/spring-bird-2295434.jpg" };
+	for (int i = 0; i < 6; i++) {
+		SDL_RWops* tmprwop = SDL_RWFromFile(ImageNames[i], "rb");
+		SDL_Surface* tmpimg = IMG_LoadJPG_RW(tmprwop);
+		image.push_back(tmpimg);
+	}
 
+	glGenTextures(6, texId);
+	for (int i = 0; i < 6; i++)
+		std::cout << texId[i] << std::endl;
+	/*SDL_Rect position;
+	position.x = 1;
+	position.y = 1;
+	position.h = 10;
+	position.w = 10;
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, SDL_CreateTextureFromSurface(renderer, image.at(0)), NULL, &position);
+	SDL_RenderPresent(renderer);*/
+	
+	/****** Main Game Loop ******/
+	while (quit == false)
+	{
+
+		/****** Get Most Current Time ******/
+		var::frame_start_time = SDL_GetTicks();
+
+		/****** Draw Rectangle ******/
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glLoadIdentity();
+		glTranslatef(0.0f, 0.0f, -6.0f);
+		glRotatef(rotation, 1.0f, 1.0f, 0.0f);
+		//errorcheck();
+
+		RenewTexture(image.at(0), 0);
+		glBegin(GL_QUADS);
+		/* Cube Top */
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		glTexCoord2f(0, 1.0); glVertex3f(-1.0f, 1.0f, 1.0f);
+		glTexCoord2f(0, 0); glVertex3f(-1.0f, 1.0f, -1.0f);
+		glTexCoord2f(1.0, 0); glVertex3f(1.0f, 1.0f, -1.0f);
+		glTexCoord2f(1.0, 1.0); glVertex3f(1.0f, 1.0f, 1.0f);
+		glEnd();
+
+		RenewTexture(image.at(1), 1);
+		/* Cube Bottom */
+		glBegin(GL_QUADS);
+		glColor4f(1.0f, 0.5f, 0.0f, 1.0f);
+		glTexCoord2f(0, 1.0); glVertex3f(-1.0f, -1.0f, 1.0f);
+		glTexCoord2f(0, 0); glVertex3f(-1.0f, -1.0f, -1.0f);
+		glTexCoord2f(1.0, 0); glVertex3f(1.0f, -1.0f, -1.0f);
+		glTexCoord2f(1.0, 1.0); glVertex3f(1.0f, -1.0f, 1.0f);
+		glEnd();
+
+		RenewTexture(image.at(2), 2);
+		/* Cube Front */
+		glBegin(GL_QUADS);
+		glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+		glTexCoord2f(0, 1.0); glVertex3f(-1.0f, 1.0f, 1.0f);
+		glTexCoord2f(0, 0); glVertex3f(1.0f, 1.0f, 1.0f);
+		glTexCoord2f(1.0, 0); glVertex3f(1.0f, -1.0f, 1.0f);
+		glTexCoord2f(1.0, 1.0); glVertex3f(-1.0f, -1.0f, 1.0f);
+		glEnd();
+
+		RenewTexture(image.at(3), 3);
+		/* Cube Back */
+		glBegin(GL_QUADS);
+		glColor4f(0.0f, 1.0f, 0.5f, 1.0f);
+		glTexCoord2f(0, 1.0); glVertex3f(-1.0f, 1.0f, -1.0f);
+		glTexCoord2f(0, 0); glVertex3f(1.0f, 1.0f, -1.0f);
+		glTexCoord2f(1.0, 0); glVertex3f(1.0f, -1.0f, -1.0f);
+		glTexCoord2f(1.0, 1.0); glVertex3f(-1.0f, -1.0f, -1.0f);
+		glEnd();
+
+		RenewTexture(image.at(4), 4);
+		/* Cube Left Side */
+		glBegin(GL_QUADS);
+		glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
+		glTexCoord2f(0, 1.0); glVertex3f(-1.0f, 1.0f, -1.0f);
+		glTexCoord2f(0, 0); glVertex3f(-1.0f, 1.0f, 1.0f);
+		glTexCoord2f(1.0, 0); glVertex3f(-1.0f, -1.0f, 1.0f);
+		glTexCoord2f(1.0, 1.0); glVertex3f(-1.0f, -1.0f, -1.0f);
+		glEnd();
+
+		RenewTexture(image.at(5), 5);
+		/* Cube Right Side */
+		glBegin(GL_QUADS);
+		glColor4f(0.15f, 0.25f, 0.75f, 1.0f);
+		glTexCoord2f(0, 1.0); glVertex3f(1.0f, 1.0f, -1.0f);
+		glTexCoord2f(0, 0); glVertex3f(1.0f, 1.0f, 1.0f);
+		glTexCoord2f(1.0, 0); glVertex3f(1.0f, -1.0f, 1.0f);
+		glTexCoord2f(1.0, 1.0); glVertex3f(1.0f, -1.0f, -1.0f);
+		glEnd();
+		errorcheck();
+		rotation -= 0.5f;
+		/****** Check for Key & System input ******/
+		while (SDL_PollEvent(&event))
+		{
+
+			/******  Application Quit Event ******/
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+
+		/****** Update Screen And Frame Counts ******/
+		SDL_GL_SwapWindow(win);
+		var::frame_count++;
+		var::frame_current_time = SDL_GetTicks();
+
+		/****** Frame Rate Handle ******/
+		if ((var::frame_current_time - var::frame_start_time) < (1000 / 60))
+		{
+			var::frame_count = 0;
+			SDL_Delay((1000 / 60) - (var::frame_current_time - var::frame_start_time));
+		}
+	}
+	clean_up();
 	return 0;
-
 }
